3bfdd5ebb675331b2f20a12e8c99511749ffd926
Martin Blazek <martinblazek6@gmail.com>
2023-12-01 20:40:23 +0100

    test fix

--- src/test/java/com/personsearcher/personsearcher/PersonSearcherApplicationTests.java
+++ src/test/java/com/personsearcher/personsearcher/PersonSearcherApplicationTests.java
@@ -3,7 +3,7 @@ package com.personsearcher.personsearcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PersonSearcherApplicationTests.class)
 class PersonSearcherApplicationTests {
 
     @Test
